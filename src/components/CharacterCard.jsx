b66d1b7818fa4a11462876d143a4471f73da3b85
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-04 20:33:54 +0200

    feat: add key to character card

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,8 +1,11 @@
 import { MISSING_CHARACTER } from "../constants/constants.jsx";
 
-const CharacterCard = ({ name, species, image }) => {
+const CharacterCard = ({ name, species, image, id }) => {
   return (
-    <div className="flex flex-col justify-between items-center mx-auto p-4 rounded-lg bg-dun shadow-md w-64 h-80 border-dun border-2 hover:border-wtfdarkbrown">
+    <div
+      className="flex flex-col justify-between items-center mx-auto p-4 rounded-lg bg-dun shadow-md w-64 h-80 border-dun border-2 hover:border-wtfdarkbrown"
+      key={id}
+    >
       <img
         src={image || MISSING_CHARACTER}
         className="rounded-lg mb-2 h-52 w-48 object-cover"
