6f4ca850eb51d1ef96aaddb93530e44197f19e1c
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-04 19:57:17 +0200

    feat: add character list

---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,17 @@
+import CharacterCard from "./CharacterCard";
+
+const CharacterList = ({ characterArray }) => {
+  return (
+    <div className="flex flex-wrap px-10 gap-y-6 gap-x-2">
+      {characterArray.map((character) => (
+        <CharacterCard
+          name={character.attributes.name}
+          image={character.attributes.image}
+          species={character.attributes.species}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CharacterList;
