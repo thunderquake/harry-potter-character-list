95c3da432b9269d9aee5da9b7cf4387e75c5cb35
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-04 19:43:56 +0200

    fix: crop image

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -5,7 +5,7 @@ const CharacterCard = ({ name, species, image }) => {
     <div className="flex flex-col justify-between items-center mx-auto p-4 rounded-lg bg-dun shadow-md w-64 h-80 border-dun border-2 hover:border-wtfdarkbrown">
       <img
         src={image || MISSING_CHARACTER}
-        className="rounded-lg mb-2 max-h-52"
+        className="rounded-lg mb-2 h-52 w-48 object-cover"
       ></img>
       <div>
         <p className="text-xl text-black font-bold">{name}</p>
