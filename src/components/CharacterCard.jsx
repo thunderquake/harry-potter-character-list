f3732d746d5e072d3cf091dbb5220a0bb1c8f23d
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-04 17:05:03 +0200

    fix: limit image size

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -4,7 +4,10 @@ const CharacterCard = ({ name, species, image }) => {
   const MISSING_CHARACTER = "https://potterdb.com/images/missing_character.svg";
   return (
     <div className="flex flex-col justify-between items-center mx-auto p-4 rounded-lg bg-dun shadow-md w-64 h-80 border-dun border-2 hover:border-wtfdarkbrown">
-      <img src={image || MISSING_CHARACTER} className="rounded-lg mb-2"></img>
+      <img
+        src={image || MISSING_CHARACTER}
+        className="rounded-lg mb-2 max-h-52"
+      ></img>
       <div>
         <p className="text-xl text-black font-bold">{name}</p>
         <p className="text-md text-gray-700">{species}</p>
