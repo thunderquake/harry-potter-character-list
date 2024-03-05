87e00e9bb3067c2b79d59d4048a46ef1b1a8cf37
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-05 18:56:02 +0200

    feat: add not found page

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import CharacterListPage from "../components/CharacterListPage";
 import ErrorPage from "../components/ErrorPage";
+import NotFoundPage from "../components/NotFoundPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <CharacterListPage />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
   },
 ]);
 
