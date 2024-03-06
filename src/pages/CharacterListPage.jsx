5d7ae8ef81e9538ce581d2c03faf5e737c579780
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-06 11:36:49 +0200

    feat: add react query fetching

--- src/pages/CharacterListPage.jsx
+++ src/pages/CharacterListPage.jsx
@@ -1,59 +1,32 @@
-import CharacterList from "../components/CharacterList";
 import hplogo from "../assets/hplogo.png";
+import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
+import HttpService from "../services/httpService";
+import CharacterList from "../components/CharacterList";
 
 const CharacterListPage = () => {
-  const character = {
-    id: "3c305e51-7129-4272-b1d6-90e25a599a2e",
-    type: "character",
-    attributes: {
-      slug: "aberforth-dumbledore",
-      alias_names: ["Ab"],
-      animagus: null,
-      blood_status: "Half-blood",
-      boggart: null,
-      born: "Between 1 September 1883 and 31 August 1884, Mould-on-the-Wold, England, Great Britain (presumably)",
-      died: null,
-      eye_color: "Bright blue",
-      family_members: [
-        "Percival Dumbledore (father) †",
-        "Kendra Dumbledore (mother) †",
-        "Albus Dumbledore (older brother) †",
-        "Ariana Dumbledore (youngest sister) †",
-        "Aurelius Dumbledore (biological son)",
-        "Honoria (aunt)",
-        "Dumbledore family",
-      ],
-      gender: "Male",
-      hair_color: "Grey",
-      height: null,
-      house: null,
-      image:
-        "https://static.wikia.nocookie.net/harrypotter/images/4/40/Aberforth_Dumbledore.jpg",
-      jobs: ["Owner and Barman of the Hog's Head Inn"],
-      marital_status: null,
-      name: "Aberforth Dumbledore",
-      nationality: "English",
-      patronus: "Goat",
-      romances: ["Aurelius Dumbledore's mother (former lover)"],
-      skin_color: null,
-      species: "Human",
-      titles: [],
-      wands: ["Unknown length, wood and core"],
-      weight: null,
-      wiki: "https://harrypotter.fandom.com/wiki/Aberforth_Dumbledore",
-    },
-    links: {
-      self: "/v1/characters/3c305e51-7129-4272-b1d6-90e25a599a2e",
-    },
-  };
+  const {
+    isLoading,
+    isError,
+    data: charactersResponse,
+  } = useQuery({
+    queryKey: "characters",
+    queryFn: () => HttpService.getCharacters(20, 1),
+  });
+  const [characters, setCharacters] = useState([]);
 
-  const didArray = new Array(20).fill(character);
+  useEffect(() => {
+    setCharacters(charactersResponse);
+  }, [charactersResponse]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching data</div>;
 
   return (
     <div className=" bg-hpbrown min-h-svh">
-      <div className="container mx-auto p-10">
-        <img src={hplogo} className="max-w-96 mx-auto pb-6"></img>
-        <CharacterList characterArray={didArray} />
+      <div className="container mx-auto py-10 px-16">
+        <img src={hplogo} className="max-w-96 mx-auto pb-8"></img>
+        {characters && <CharacterList characterArray={characters} />}
       </div>
     </div>
   );
