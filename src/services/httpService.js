ea19068194f4ea49730e397d61cde1232562689c
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-06 11:35:16 +0200

    feat: add http service

---
+++ src/services/httpService.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+class HttpService {
+  constructor(baseURL = "https://api.potterdb.com/v1") {
+    this.baseUrl = baseURL;
+    this.instance = axios.create({ baseURL: this.baseUrl });
+  }
+
+  async getCharacters(pageSize, pageNumber) {
+    try {
+      const response = await this.instance.get("/characters", {
+        params: {
+          "page[size]": pageSize,
+          "page[number]": pageNumber,
+        },
+      });
+      return response.data.data;
+    } catch (error) {
+      throw new Error("Failed to fetch characters");
+    }
+  }
+}
+
+export default new HttpService();
