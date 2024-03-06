adc87d9352bbc0f1dd372006db24d1d12e18c580
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-06 11:36:49 +0200

    feat: add react query fetching

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,14 +2,14 @@ import { MISSING_CHARACTER } from "../constants/constants.jsx";
 
 const CharacterCard = ({ name, species, image }) => {
   return (
-    <div className="flex flex-col justify-between items-center p-4 rounded-lg bg-dun shadow-md w-64 h-80 border-dun border-2 hover:border-hpdarkbrown">
+    <div className="flex flex-col justify-between items-center p-4 rounded-lg bg-dun shadow-md w-64 h-80 hover:shadow-lg hover:shadow-yellow-700 hover:ring hover:ring-yellow-500">
       <img
         src={image || MISSING_CHARACTER}
         className="rounded-lg mb-2 h-52 w-48 object-cover"
       ></img>
-      <div>
-        <p className="text-xl text-black font-bold">{name}</p>
-        <p className="text-md text-gray-700">{species}</p>
+      <div className="flex flex-col max-w-full items-center justify-center">
+        <p className="text-xl text-center text-hpdarkbrown font-bold">{name}</p>
+        <p className="text-md text-yellow-900">{species}</p>
       </div>
     </div>
   );
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -2,13 +2,13 @@ import CharacterCard from "./CharacterCard";
 
 const CharacterList = ({ characterArray }) => {
   return (
-    <div className="flex flex-wrap items-center justify-center gap-4">
+    <div className="flex flex-wrap items-center justify-center gap-8">
       {characterArray.map((character) => (
         <CharacterCard
           name={character.attributes.name}
           image={character.attributes.image}
           species={character.attributes.species}
-          key={character.attributes.id}
+          key={character.id}
         />
       ))}
     </div>
