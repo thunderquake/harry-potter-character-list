ad9a930eabfc75d2978f0255c9c9d1d8a211ed20
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-10 11:57:06 +0200

    fix: fix character card style

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,13 +2,15 @@ import { MISSING_CHARACTER } from "../constants/constants.jsx";
 
 const CharacterCard = ({ name, species, image }) => {
   return (
-    <div className="flex flex-col justify-between items-center p-4 rounded-lg bg-dun shadow-md w-64 h-80 hover:shadow-lg hover:shadow-yellow-700 hover:ring hover:ring-yellow-500">
+    <div className="flex flex-col justify-between items-center  rounded-lg bg-dun shadow-md w-64 h-80 hover:shadow-lg hover:shadow-yellow-700 hover:ring hover:ring-yellow-500">
       <img
         src={image || MISSING_CHARACTER}
-        className="rounded-lg mb-2 h-52 w-48 object-cover"
+        className="rounded-t-lg mb-2 min-h-52 max-h-52 w-full object-cover"
       ></img>
-      <div className="flex flex-col max-w-full items-center justify-center">
-        <p className="text-xl text-center text-hpdarkbrown font-bold">{name}</p>
+      <div className="flex flex-col h-36 p-2 items-center justify-center">
+        <p className="text-xl text-center text-hpdarkbrown font-bold text-ellipsis">
+          {name}
+        </p>
         <p className="text-md text-yellow-900">{species}</p>
       </div>
     </div>
