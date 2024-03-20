1079505401cb6f6fcfa3602ebe266713c1f4b1b8
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-20 19:58:01 +0200

    fix: fix styles

--- src/components/CharacterInfo.jsx
+++ src/components/CharacterInfo.jsx
@@ -2,28 +2,30 @@ import { MISSING_CHARACTER } from "../constants/constants.jsx";
 
 const CharacterInfo = ({ image, name, species, bloodStatus, house }) => {
   return (
-    <div className="container mx-auto py-10 px-16">
-      <img
-        src={image || MISSING_CHARACTER}
-        className="rounded-full mb-2 min-h-52 max-h-52 w-full object-cover"
-      ></img>
-      <div className="flex flex-col gap-2">
-        <p className="text-xl text-hpdarkbrown font-bold">Name</p>
-        <p className="text-xl text-hpdarkbrown">{name || "Unknown"}</p>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p className="text-xl text-hpdarkbrown font-bold">Species</p>
-        <p className="text-xl text-hpdarkbrown">{species || "Unknown"}</p>
-      </div>
+    <div className="container w-fit mx-auto content-center py-10 px-16">
+      <div className="flex flex-col justify-center items-center gap-4 p-6 w-fit rounded-lg bg-dun shadow-lg shadow-yellow-700 ring ring-yellow-500">
+        <img
+          src={image || MISSING_CHARACTER}
+          className="object-cover w-52 h-52 rounded-full mb-5 shadow-lg"
+        ></img>
+        <div className="flex flex-col gap-2 items-center">
+          <p className="text-xl text-hpdarkbrown font-bold">Name</p>
+          <p className="text-xl text-hpdarkbrown">{name || "Unknown"}</p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <p className="text-xl text-hpdarkbrown font-bold">Species</p>
+          <p className="text-xl text-hpdarkbrown">{species || "Unknown"}</p>
+        </div>
 
-      <div className="flex flex-col gap-2">
-        <p className="text-xl text-hpdarkbrown font-bold">Blood status</p>
-        <p className="text-xl text-hpdarkbrown">{bloodStatus || "Unknown"}</p>
-      </div>
+        <div className="flex flex-col gap-2 items-center">
+          <p className="text-xl text-hpdarkbrown font-bold">Blood status</p>
+          <p className="text-xl text-hpdarkbrown">{bloodStatus || "Unknown"}</p>
+        </div>
 
-      <div className="flex flex-col gap-2">
-        <p className="text-xl text-hpdarkbrown font-bold">House</p>
-        <p className="text-xl text-hpdarkbrown">{house || "Unknown"}</p>
+        <div className="flex flex-col gap-2 items-center">
+          <p className="text-xl text-hpdarkbrown font-bold">House</p>
+          <p className="text-xl text-hpdarkbrown">{house || "Unknown"}</p>
+        </div>
       </div>
     </div>
   );
