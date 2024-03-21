807c08e815f7e10f7955330ae942b5afa735cf2b
Dmytriieva Anna <loooverrr12@gmail.com>
2024-03-21 19:17:57 +0200

    feat: add go back button

--- src/components/CharacterInfo.jsx
+++ src/components/CharacterInfo.jsx
@@ -1,31 +1,32 @@
 import { MISSING_CHARACTER } from "../constants/constants.jsx";
 
-const CharacterInfo = ({ image, name, species, bloodStatus, house }) => {
+const CharacterInfo = (character) => {
+  const characterData = {
+    Name: character.name,
+    Species: character.species,
+    BloodStatus: character.blood_status,
+    House: character.house,
+  };
+
   return (
-    <div className="container w-fit mx-auto content-center py-10 px-16">
+    <div className="container w-fit mx-auto content-center py-2 px-16">
       <div className="flex flex-col justify-center items-center gap-4 p-6 w-fit rounded-lg bg-dun shadow-lg shadow-yellow-700 ring ring-yellow-500">
         <img
-          src={image || MISSING_CHARACTER}
+          src={character.image || MISSING_CHARACTER}
           className="object-cover w-52 h-52 rounded-full mb-5 shadow-lg"
         ></img>
-        <div className="flex flex-col gap-2 items-center">
-          <p className="text-xl text-hpdarkbrown font-bold">Name</p>
-          <p className="text-xl text-hpdarkbrown">{name || "Unknown"}</p>
-        </div>
-        <div className="flex flex-col gap-2 items-center">
-          <p className="text-xl text-hpdarkbrown font-bold">Species</p>
-          <p className="text-xl text-hpdarkbrown">{species || "Unknown"}</p>
-        </div>
-
-        <div className="flex flex-col gap-2 items-center">
-          <p className="text-xl text-hpdarkbrown font-bold">Blood status</p>
-          <p className="text-xl text-hpdarkbrown">{bloodStatus || "Unknown"}</p>
-        </div>
-
-        <div className="flex flex-col gap-2 items-center">
-          <p className="text-xl text-hpdarkbrown font-bold">House</p>
-          <p className="text-xl text-hpdarkbrown">{house || "Unknown"}</p>
-        </div>
+        {Object.entries(characterData).map(([property, value]) => {
+          return (
+            <div className="flex flex-col gap-2 items-center">
+              <p className="text-xl text-hpdarkbrown font-bold text-center">
+                {property}
+              </p>
+              <p className="text-xl text-hpdarkbrown text-center">
+                {value || "Unknown"}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/components/GoBackButton.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
+const GoBackButton = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
+
+  return (
+    <div className="flex justify-center">
+      <button
+        className="bg-dun shadow-md m-4 text-hpdarkbrown font-bold py-2 px-4 rounded-full hover:shadow-lg hover:shadow-yellow-700 hover:ring hover:ring-yellow-500"
+        onClick={goBack}
+      >
+        Go Back
+      </button>
+    </div>
+  );
+};
+
+export default GoBackButton;
