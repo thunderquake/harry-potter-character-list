755b83efd990363fff36b7a2fb7be540ed88cd6e
Dmytriieva Anna <loooverrr12@gmail.com>
2024-04-28 19:29:06 +0300

    feat: add pagination

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,2 +1,4 @@
 export const MISSING_CHARACTER =
   "https://potterdb.com/images/missing_character.svg";
+
+export const CHARACTERS_PAGE_LIMIT = 20;
--- src/pages/CharacterListPage.jsx
+++ src/pages/CharacterListPage.jsx
@@ -4,26 +4,39 @@ import { useState, useEffect } from "react";
 import characterService from "../services/characterService";
 import CharacterList from "../components/CharacterList";
 import PaginationButtons from "../components/PaginationButtons";
+import { useSearchParams } from "react-router-dom";
+import { CHARACTERS_PAGE_LIMIT } from "../constants/constants";
 
 const CharacterListPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [page, setPage] = useState(1);
+
   const {
     isLoading,
     isError,
     data: charactersResponse,
+    refetch,
   } = useQuery({
-    queryKey: "characters",
-    queryFn: () => characterService.getCharacters(20, 1),
+    queryKey: ["characters", Number(searchParams.get("page"))],
+    queryFn: () =>
+      characterService.getCharacters(
+        20,
+        searchParams.get("page") ? Number(searchParams.get("page")) : 1
+      ),
   });
+
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    setCharacters(charactersResponse);
+    setCharacters(charactersResponse ? charactersResponse.data : []);
   }, [charactersResponse]);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error fetching data</div>;
 
-  const pageCount = 20;
+  console.log();
+  const recordsCount = charactersResponse.meta.pagination.records;
+  const pageCount = Math.ceil(recordsCount / CHARACTERS_PAGE_LIMIT);
 
   return (
     <div className=" bg-hpbrown min-h-svh">
@@ -31,7 +44,12 @@ const CharacterListPage = () => {
         <img src={hplogo} className="max-w-full w-96 mx-auto pb-8"></img>
         {characters && <CharacterList characterArray={characters} />}
       </div>
-      <PaginationButtons pageCount={pageCount} />
+      <PaginationButtons
+        pageCount={pageCount}
+        searchParams={searchParams}
+        setSearchParams={setSearchParams}
+        setPage={setPage}
+      />
     </div>
   );
 };
