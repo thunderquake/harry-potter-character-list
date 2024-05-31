43f061704410f60f8514ccab5a9ac4c99498b7c7
Dmytriieva Anna <loooverrr12@gmail.com>
2024-05-31 17:29:08 +0300

    feat: add clear button style

--- src/components/RemoveFiltersButton.jsx
+++ src/components/RemoveFiltersButton.jsx
@@ -4,7 +4,16 @@ const RemoveFiltersButton = ({ setSearchParams, setSearchTerm }) => {
     setSearchParams({ page: 1 });
   };
 
-  return <button onClick={handleClick}>Hello</button>;
+  return (
+    <div className="max-w-full flex justify-center">
+      <button
+        onClick={handleClick}
+        className="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Clear filters!
+      </button>
+    </div>
+  );
 };
 
 export default RemoveFiltersButton;
